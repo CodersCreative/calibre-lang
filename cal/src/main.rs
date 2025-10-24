1989645a15d734dab5b408a373d054522fbe4909
Creative Coders <113299264+CodersCreative@users.noreply.github.com>
2025-10-24 02:51:43 +0200

    Made repl automatically start if a repl file is detected

--- cal/src/main.rs
+++ cal/src/main.rs
@@ -5,7 +5,7 @@ use clap::Parser;
 use rustyline::{DefaultEditor, error::ReadlineError};
 use std::{error::Error, fs, io::Write, path::PathBuf, str::FromStr};
 
-fn repl() -> Result<(), Box<dyn Error>> {
+fn repl(file: Option<&str>) -> Result<(), Box<dyn Error>> {
     let mut env = InterpreterEnvironment::new();
     let mut checker = CheckerEnvironment::new();
     let mut tokenizer = Tokenizer::default();
@@ -15,8 +15,91 @@ fn repl() -> Result<(), Box<dyn Error>> {
     let mut editor = DefaultEditor::new()?;
     let mut logical_history = Vec::new();
 
+    if let Some(file) = file {
+        logical_history.push(format!("LOAD {}", file));
+
+        match perform_repl_line(
+            &mut env,
+            &mut checker,
+            &scope,
+            &checker_scope,
+            &mut parser,
+            &mut tokenizer,
+            &logical_history,
+            logical_history.last().unwrap().clone(),
+        ) {
+            None => {}
+            Some(RuntimeValue::Null) | Some(RuntimeValue::Ref(_, _)) => {}
+            Some(val) => {
+                println!("{}", val.to_string());
+            }
+        }
+        println!("Successfully Loaded : {}", file);
+    }
+
+    loop {
+        let readline = editor.readline(">> ");
+        match readline {
+            Ok(line) => {
+                editor.add_history_entry(line.clone())?;
+
+                match perform_repl_line(
+                    &mut env,
+                    &mut checker,
+                    &scope,
+                    &checker_scope,
+                    &mut parser,
+                    &mut tokenizer,
+                    &logical_history,
+                    line.clone(),
+                ) {
+                    None => continue,
+                    Some(RuntimeValue::Null) | Some(RuntimeValue::Ref(_, _)) => {}
+                    Some(val) => {
+                        println!("{}", val.to_string());
+                    }
+                }
+
+                logical_history.push(line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn perform_repl_line(
+    env: &mut InterpreterEnvironment,
+    checker: &mut CheckerEnvironment,
+    scope: &u64,
+    checker_scope: &u64,
+    parser: &mut calibre_parser::Parser,
+    tokenizer: &mut Tokenizer,
+    logical_history: &[String],
+    line: String,
+) -> Option<RuntimeValue> {
     let mut perform_line = |line: String| -> Option<RuntimeValue> {
-        let program = parser.produce_ast(tokenizer.tokenize(line).unwrap());
+        let tokens = match tokenizer.tokenize(line) {
+            Ok(x) => x,
+            Err(e) => {
+                eprintln!("Unable to parse input due to : {:?}.\nPlease Retry.", e);
+                return None;
+            }
+        };
+
+        let program = parser.produce_ast(tokens);
 
         if !parser.errors.is_empty() {
             eprintln!(
@@ -66,80 +149,77 @@ fn repl() -> Result<(), Box<dyn Error>> {
         }
         Some(val)
     };
+    if let Some(x) = line.split_whitespace().nth(0) {
+        match x.trim() {
+            "SAVE" => {
+                let path = line.split_whitespace().nth(1).unwrap_or("repl.cl");
 
-    loop {
-        let readline = editor.readline(">> ");
-        match readline {
-            Ok(line) => {
-                editor.add_history_entry(line.clone())?;
-                if let Some(x) = line.split_whitespace().nth(0) {
-                    match x.trim() {
-                        "SAVE" => {
-                            logical_history.push(line.clone());
-                            let path = line.split_whitespace().nth(1).unwrap_or("repl.cl");
-
-                            let mut file = fs::File::create(&path)?;
-                            let mut txt = String::from(
-                                "/*CLREPL\nCALIBRE LANG REPL FILE.\nCan only be run by the REPL using the 'LOAD' keyword*/",
-                            );
-
-                            for line in logical_history.iter() {
-                                txt.push_str(&format!("\n{line}"));
-                            }
-
-                            let _ = file.write_all(txt.as_bytes())?;
-
-                            continue;
-                        }
-                        "LOAD" => {
-                            logical_history.push(line.clone());
-                            let contents = fs::read_to_string(
-                                line.split_whitespace().nth(1).unwrap_or("repl.cl"),
-                            )?;
-
-                            for line in contents.lines().skip(3) {
-                                if let Some(x) = line.split_whitespace().nth(0) {
-                                    match x.trim() {
-                                        "SAVE" | "LOAD" => continue,
-                                        _ => {}
-                                    }
-                                }
-
-                                let _ = perform_line(line.to_string());
-                            }
-
-                            continue;
-                        }
-                        _ => {}
+                let mut file = match fs::File::create(&path) {
+                    Ok(x) => x,
+                    Err(e) => {
+                        eprintln!("Error Saving : {e}");
+                        return None;
                     }
+                };
+                let mut txt = String::from("/*CLREPL\nCALIBRE LANG REPL FILE.*/\n");
+
+                for line in logical_history.iter() {
+                    txt.push_str(&format!("\n{line}"));
                 }
 
-                match perform_line(line.clone()) {
-                    None => continue,
-                    Some(RuntimeValue::Null) | Some(RuntimeValue::Ref(_, _)) => {}
-                    Some(val) => {
-                        println!("{}", val.to_string());
+                let _ = match file.write_all(txt.as_bytes()) {
+                    Ok(x) => x,
+                    Err(e) => {
+                        eprintln!("Error Saving : {e}");
+                        return None;
                     }
+                };
+
+                return Some(RuntimeValue::Null);
+            }
+            "LOAD" => {
+                let contents =
+                    match fs::read_to_string(line.split_whitespace().nth(1).unwrap_or("repl.cl")) {
+                        Ok(x) => x,
+                        Err(e) => {
+                            eprintln!("Error Loading : {e}");
+                            return None;
+                        }
+                    };
+                let mut val = RuntimeValue::Null;
+                for line2 in contents.lines().skip(3) {
+                    if let Some(x) = line2.split_whitespace().nth(0) {
+                        match x.trim() {
+                            "SAVE" => continue,
+                            "LOAD"
+                                if line2.split_whitespace().nth(1)
+                                    == line.split_whitespace().nth(1) =>
+                            {
+                                continue;
+                            }
+                            _ => {}
+                        }
+                    }
+                    val = perform_repl_line(
+                        env,
+                        checker,
+                        scope,
+                        checker_scope,
+                        parser,
+                        tokenizer,
+                        logical_history,
+                        line2.to_string(),
+                    )
+                    .unwrap_or(RuntimeValue::Null);
                 }
 
-                logical_history.push(line);
+                return Some(val);
             }
-            Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
-                break;
-            }
-            Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
-                break;
-            }
-            Err(err) => {
-                println!("Error: {}", err);
-                break;
-            }
+            _ => {}
         }
     }
 
-    Ok(())
+    perform_line(line)
 }
 
 fn file(
@@ -154,7 +234,8 @@ fn file(
     let mut tokenizer = Tokenizer::default();
     let contents = fs::read_to_string(path)?;
     if contents.starts_with("/*CLREPL") {
-        return Err(String::from("Cannot run file using --path. It can only be run in the REPL using the 'LOAD' keyword.").into());
+        println!("File is a REPL file switching to REPL.");
+        return repl(Some(path));
     }
 
     let program = parser.produce_ast(tokenizer.tokenize(contents).unwrap());
@@ -202,6 +283,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     if let Some(path) = args.path {
         file(&path, Vec::new())
     } else {
-        repl()
+        repl(None)
     }
 }
